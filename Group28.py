f3c61618d145ad9b8274c51777694a201e35f1b1
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2020-12-03 12:20:54 +0000

    Add function for end-to-end. Output not correct

--- Group28.py
+++ Group28.py
@@ -25,11 +25,19 @@ def load_table(file):
     return data_input
 
 # calculate means
-def column_means(data)
-    data_output = data.mean(axis=0)
+def column_means(data):
+    data_output = data.mean(axis=1, skipna=True)
     return data_output
 
 # save new csv
 def save_output(data, file):
     data.to_csv(file)
     print('File saved to ', file)
+
+# implement full workflow in one function
+def load_run_save(dirpath, infilename, outfilename):
+    input_path = filepath(dirpath, infilename)
+    output_path = filepath(dirpath, outfilename)
+    data_input = load_table(input_path)
+    data_output = column_means(data_input)
+    save_output(data_output, outfilename)
