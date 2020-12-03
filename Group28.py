0281c58254249ed21454913ad313b6bacae0c4bb
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2020-12-03 11:01:07 +0000

    Create skeleton file with details of functions

---
+++ Group28.py
@@ -0,0 +1,13 @@
+# ===================================
+# Calculate mean of columns in .csv
+# ===================================
+
+# Functions to build: 
+#   specify filepath
+#   read csv
+#   calculate mean of columns
+#   save new csv
+
+# specify imports
+from Pathlib import Path
+import numpy as np
